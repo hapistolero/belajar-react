0cab6235eaed838829138970ecd96a911bbb32b8
hapistolero <ilhamhardanz2507@gmail.com>
2023-06-23 21:15:54 +0700

    stateful and stateless component

---
+++ src/component/hellocomponent.jsx
@@ -0,0 +1,6 @@
+import React from  'react'
+import './hellocomponent.css'
+// stateless component
+export const HelloComponent =() =>{
+    return(<div className='text'>ambatukam</div>)
+  }
---
+++ src/container/statefulcompo.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+
+// statefull component
+export class StatefulCompo extends Component{
+    render(){
+      return <div>aku sayang kamu</div>
+    }
+  }
